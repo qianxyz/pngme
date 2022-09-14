e22d9bacb014dd08bd71b8345a26db5e33e8075c
Qianxy <xyqian67@gmail.com>
2022-09-15 00:59:16 +0800

    Refactor subcommand args into struct

--- src/args.rs
+++ src/args.rs
@@ -3,7 +3,7 @@
 use std::path::PathBuf;
 use std::str::FromStr;
 
-use clap::{Parser, Subcommand};
+use clap::{Args, Parser, Subcommand};
 
 use crate::chunk_type::ChunkType;
 
@@ -26,44 +26,56 @@ pub struct Cli {
 #[derive(Subcommand, Debug)]
 enum Commands {
     /// Encode message into PNG file
-    Encode {
-        /// The PNG file to be encoded into
-        file_path: PathBuf,
-
-        /// The secret key (four alphabets)
-        #[clap(value_parser = parse_chunk_type)]
-        chunk_type: ChunkType,
-
-        /// The secret message
-        message: String,
-
-        /// Optional output file path
-        output_file: Option<PathBuf>,
-    },
+    Encode(EncodeArgs),
 
     /// Decode message from PNG file
-    Decode {
-        /// The PNG file to be decoded
-        file_path: PathBuf,
-
-        /// The secret key (four alphabets)
-        #[clap(value_parser = parse_chunk_type)]
-        chunk_type: ChunkType,
-    },
+    Decode(DecodeArgs),
 
     /// Remove a chunk from PNG file
-    Remove {
-        /// The PNG file to be operated on
-        file_path: PathBuf,
-
-        /// The secret key (four alphabets)
-        #[clap(value_parser = parse_chunk_type)]
-        chunk_type: ChunkType,
-    },
+    Remove(RemoveArgs),
 
     /// Print PNG file as data stream
-    Print {
-        /// The PNG file to be printed
-        file_path: PathBuf,
-    },
+    Print(PrintArgs),
+}
+
+#[derive(Args, Debug)]
+struct EncodeArgs {
+    /// The PNG file to be encoded into
+    file_path: PathBuf,
+
+    /// The secret key (four alphabets)
+    #[clap(value_parser = parse_chunk_type)]
+    chunk_type: ChunkType,
+
+    /// The secret message
+    message: String,
+
+    /// Optional output file path
+    output_file: Option<PathBuf>,
+}
+
+#[derive(Args, Debug)]
+struct DecodeArgs {
+    /// The PNG file to be decoded
+    file_path: PathBuf,
+
+    /// The secret key (four alphabets)
+    #[clap(value_parser = parse_chunk_type)]
+    chunk_type: ChunkType,
+}
+
+#[derive(Args, Debug)]
+struct RemoveArgs {
+    /// The PNG file to be operated on
+    file_path: PathBuf,
+
+    /// The secret key (four alphabets)
+    #[clap(value_parser = parse_chunk_type)]
+    chunk_type: ChunkType,
+}
+
+#[derive(Args, Debug)]
+struct PrintArgs {
+    /// The PNG file to be printed
+    file_path: PathBuf,
 }
